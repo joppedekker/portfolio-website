ef24a4dcbffce958b56783aef646c2ba216e40f1
Joppe Dekker <>
2018-05-30 22:26:42 +0200

    Changed the last section to footer deleted himeji

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ $('a[href^="#"]').on('click', function (event) {
 
   if (target.length) {
     event.preventDefault()
-    $('html,body').stop().animate({
+    $('html ,body').stop().animate({
       scrollTop: target.offset().top
     }, 1000)
   }
